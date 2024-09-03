262d5f9506dfa29591498e6a069b48dca7367e8a
marsacolombo <aymeric.biaou.ext@melting-point.fr>
2024-09-03 12:55:26 +0200

    1-animal-logic : feat: animal logic (CRUD, migration, bo, first controllers)

---
+++ src/Entity/Owner.php
@@ -0,0 +1,73 @@
+<?php  
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+
+#[ORM\Entity]
+class Owner
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "string", length: 255)]
+    private $name;
+
+    #[ORM\OneToMany(targetEntity: Animal::class, mappedBy: "owner")]
+    private $animals;
+
+    public function __construct()
+    {
+        $this->animals = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return Collection|Animal[]
+     */
+    public function getAnimals(): Collection
+    {
+        return $this->animals;
+    }
+
+    public function addAnimal(Animal $animal): self
+    {
+        if (!$this->animals->contains($animal)) {
+            $this->animals[] = $animal;
+            $animal->setOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnimal(Animal $animal): self
+    {
+        if ($this->animals->removeElement($animal)) {
+            // set the owning side to null (unless already changed)
+            if ($animal->getOwner() === $this) {
+                $animal->setOwner(null);
+            }
+        }
+
+        return $this;
+    }
+}
